d0ce9851394dbb7b2f5fec73b73bb15d2c9fed55
hoangfamm <hoangfamm@gmail.com>
2022-06-06 11:20:53 +0700

    Update Post

--- community.js
+++ community.js
@@ -1,6 +1,6 @@
 function displayData() {
     var name = document.getElementById("name");
-var content = document.getElementById("content");
+    var content = document.getElementById("content");
     document.getElementById("namePost").innerHTML= name;
     document.getElementById("contentPost").innerHTML = content;
 }
